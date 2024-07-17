65154c924b3b295d6794deffb0b12887e897b214
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-07-17 10:34:58 -0300

    feature: exercicio finalizado

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,27 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve inserir dois comentários', () => {
+        render(<PostComment/>);
+        const btnSubmit = screen.getByTestId('btn-submit');
+        const txtArea = screen.getByTestId('txt-area');
+
+        // Alteração do estado e inserção do primeiro comentário
+        fireEvent.change(txtArea, {target: {value: "Primeiro comentário"}});
+        fireEvent.click(btnSubmit);
+
+        // Alteração do estado e inserção do segundo comentário
+        fireEvent.change(txtArea, {target: {value: "Segundo comentário"}});
+        fireEvent.click(btnSubmit);
+
+        // Validação das inserções
+        expect(screen.getByText('Primeiro comentário')).toBeInTheDocument();
+        expect(screen.getByText('Segundo comentário')).toBeInTheDocument();
     });
 });
